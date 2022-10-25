d68538714c1ee81b7fe00c651784cdbb5ad8eb28
shinwoocheol <swc9704@gmail.com>
2022-10-25 11:04:33 +0900

    Hello 프로젝트 추가함

---
+++ Hello/src/test/main/MainClass2.java
@@ -0,0 +1,21 @@
+// 이 클래스가 속해있는 패키지명을 밝혀 주어야 한다.
+package test.main;
+/*
+ * 여러줄
+ * 주석
+ */
+
+// 공개된(모든 패키지에서 사요할수 있는) 클래스, 클래스명은  MainClass2
+public class MainClass2 {
+	// 한줄 주석
+	
+	/*
+	 * 메소드명 : main
+	 * run 했을때 실행이 시작되는 아주 특별한 메소드
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}//클래스의 끝
