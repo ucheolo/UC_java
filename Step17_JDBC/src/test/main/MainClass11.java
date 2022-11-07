6d085f8ac2eb96c18def7ff6d51163f975b594da
shinwoocheol <swc9704@gmail.com>
2022-11-07 12:25:38 +0900

    Step17_JDBC 예제 추가

--- Step17_JDBC/src/test/main/MainClass11.java
+++ Step17_JDBC/src/test/main/MainClass11.java
@@ -3,46 +3,33 @@ package test.main;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 
-import test.dto.MemberDto;
 import test.util.DBConnect;
 
-public class MainClass10 {
+public class MainClass11 {
 	public static void main(String[] args) {
-		//수정할 회원의 정보
-		int num = 1;
-		String name = "호빵";
-		String addr = "분식집";
+		// 삭제할 회원의 번호라고 하자
+		int num = 4;
 		
-		//추가할 회원의 정보를 MemberDto객체에 담아서
-		//MemberDto dto = new MemberDto(num, name, addr );
-		MemberDto dto = new MemberDto();
-		dto.setName(name);
-		dto.setAddr(addr);
-		dto.setNum(num);
-		
-		update(dto);
+		delete(num);
 	}
 	
-	//회원 한명의 정보를 수정하는 메소드
-	public static void update(MemberDto dto) {
+	//인자로 전달한 번호에 해당하는 회원 한명의 정보를 삭제하는 메소드
+	public static void delete(int num) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		try {
 			// Connection 객체의 참조값 얻어오기
 			conn = new DBConnect().getConn();
 			// 실행할 미완성의 sql 문
-			String sql="UPDATE member" 
-					+ " SET name=?, addr=?" 
+			String sql="DELETE FROM member" 
 					+ " WHERE num=?";
 			// PreparedStatement 객체의 참조값 얻어오기
 			pstmt = conn.prepareStatement(sql);
 			// ? 에 값을 바인딩해서 미완성의 sql 문을 완성 시킨다.
-			pstmt.setString(1, dto.getName());
-			pstmt.setString(2, dto.getAddr());
-			pstmt.setInt(3, dto.getNum());
+			pstmt.setInt(1, num);
 			// sql 문 실행하기
 			pstmt.executeUpdate();
-			System.out.println("회원 정보를 추가했습니다.");
+			System.out.println("회원 정보를 삭제했습니다.");
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
